7e5ed4b58cd4c58845ee1519fef52988d99c9252
saraferrantini <sara.ferrantini@gmail.com>
2024-05-03 10:46:06 +0200

    primo commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title(); ?></title>
+
+    <!-- aggancio al css -->
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/style.css">
+
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <nav>
+            <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+        </nav>
+    </header>
+
---
+++ page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+
+<div id="content" class="pagina">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+        </article>
+    <?php endwhile; endif; ?>
+</div>
+
+<?php get_footer(); ?>
