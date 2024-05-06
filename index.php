cfb6b331b16ac2024a96b73416fd4f1e90cb8a72
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-05-06 17:25:41 +0200

    card place end

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 <div class="container-fluid my-5 py-5">
     <div class="row">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <div class="col-12 col-md-4 my-3">
-                <a href="<?php the_permalink()?>" class="card cardPlace" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');">
+            <div class="col-12 col-sm-6 col-md-4 my-3">
+                <a href="<?php the_permalink()?>" class="card cardPlace" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>'); background-size: cover;">
                     <div class="card-body namePlace">
                         <h2 class="card-title text-center text-uppercase titlePlace"><?php the_title(); ?></h2>
                     </div>
