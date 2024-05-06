419900341dd23b6750129c91649bc8ec89af0231
MarquisPeroni <marco_p2009@live.it>
2024-05-06 16:31:28 +0200

    secondcommit

--- single.php
+++ single.php
@@ -4,13 +4,25 @@ get_header();
 
 <?php the_post() ?>
 
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-
-    <?php if (is_single()) : ?>
-    <div>
-    
+<div class="container my-4">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card border-primary">
+                <div class="card-body text-center">
+                    <h2 class="card-title"><?php the_title(); ?></h2>
+                    <div class="mt-4">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-<?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
+
+
+
+
+
+
