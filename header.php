70da43ce47205fb43c59bd1164531ade526a774c
MarquisPeroni <marco_p2009@live.it>
2024-05-08 09:26:20 +0200

    fifthcommit

--- header.php
+++ header.php
@@ -26,14 +26,6 @@
                         value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
                 </div>
             </form>
-
-            <form role="search" method="get" id="searchform" class="search-box" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <div>
-        <label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-    </div>
-</form>
-
             <!-- Search end -->
         </nav>
     </header>
